5b061e6a670b6e3408ef0e0e18363ac47f4d91c4
warrendlee1 <warrendlee1@gmail.com>
2018-11-11 18:15:47 -0800

    new

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, render_template, send_from_directory
+import json
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def home_page():
+  return send_from_directory('LinkCru', 'index.html')
+
+@app.route('/<path:path>')
+def pages(path):
+  return send_from_directory('LinkCru', path)
+
+if __name__ == '__main__':
+  app.run()
